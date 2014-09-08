ea5a9319d87c6e1dcf34ca5713865a50aed1ea51
Sergey Vorobey <sev@qapint.com>
2014-09-08 22:17:28 +0300

    add(posts)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Posts::Application.config.secret_key_base = 'f2f7cacd45ec641e57acd23fb93e52f4b3ada1c1af48e7772a313985fa23cd943f69f8f6bb100c80d8dc9bd5a30986d23e33bf0554ee09a2ddd0a2609dd393ca'
